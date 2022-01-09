026e3c9ee2bff5ab94878a6c1969c7e9d5db915f
Anastasia Sh <91656499+iridium951@users.noreply.github.com>
2022-01-09 18:38:46 +0100

    Update snowflakes.js

--- snowflakes.js
+++ snowflakes.js
@@ -1,27 +1,57 @@
 let snowflakes = []; // array to hold snowflake objects
-let bg, maskshape;
+let bg, maskshape; // declare images
+let isMousePressed = false;
+let stage = 'init'; // init, shake, fall, fallslow
+
+let fakeTimer = 1;
+
 
 function setup() {
-  bg = loadImage('images/snowglobe.png')
+  bg = loadImage('images/snowglobe.png') 
   maskshape = loadImage('images/Subtract.png')
-  createCanvas(814, 967);
+  createCanvas(814, 967); // background image and canvas have to be the same size
   for (let i=0; i<400; i++) {
-    snowflakes.push({x: random(0, width), y: random(-600,30), d: random(2,6)}) // append snowflake objects 
+    snowflakes.push({x: random(0, width), y: random(-550,30), d: random(2,6)}) // append snowflake objects
   }
-  noStroke();
+  noStroke(); //removes stroke effect around snowflakes
+  console.log('s');
 }
 
+
 function draw() {
-  
   background(bg);
+  if(isMousePressed) {
+    if(stage =='init') {
+      //shake
+      fakeTimer++;
+      console.log('t');
+      if(fakeTimer > 600) {
+        stage = 'fall';
+      }
+    }
+    if(stage =='fall') {
+      oneFallDownStep();
+    }
+  }
+}
+
+function mousePressed() {
+  console.log('x');
+  isMousePressed = true;
+}
+
+function oneFallDownStep() {
   for (let i=0; i<400; i++) {
     let snowflake = snowflakes[i]
-    snowflake.y += random(1, 5)
+    snowflake.y += random(1, 4)
     if(snowflake.y> 540) {
       snowflake.y = 0
       snowflake.x = random(0, width)
     } 
     circle(snowflake.x, snowflake.y, snowflake.d)
   }
-  image(maskshape, 0, 0);
+  image(maskshape, 0, 0); // Top-left corner of the img is at (0, 0)
 }
+
+//function shake() {
+  
